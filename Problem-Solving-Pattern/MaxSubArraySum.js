791540c80631a0b192efb36f527ee4f247326136
zanderthuo <thuozander@gmail.com>
2022-07-01 20:10:06 +0300

    Started recursion section

---
+++ Problem-Solving-Pattern/MaxSubArraySum.js
@@ -0,0 +1,15 @@
+/** 
+ * A function which accepts an array of integers and a number called .
+ * The function should calculate the maximum sum of n consecutive
+ * elements in the array
+ *
+ */
+
+/**
+ * Example
+ * maxSubarraySum([1,2,5,2,8,1,5], 2) // 10
+ * maxSubarraySum([1,2,5,2,8,1,5], 4) // 17
+ * maxSubarraySum([4,2,1,6], 1) // 6
+ * maxSubarraySum([4,2,1,6,2], 4) // 10
+ * maxSubarraySum([], 4) // null
+ */
---
+++ Recursion/CountDown.js
@@ -0,0 +1,8 @@
+const countDown = (num) => {
+    for (let i = num; i > 0; i--) {
+        console.log(i)
+    }
+    console.log("All done!")
+}
+
+countDown(5)
