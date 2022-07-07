b9af3945f644a11379be0055a3353c304abf70a5
zanderthuo <thuozander@gmail.com>
2022-07-07 23:04:37 +0300

    Finished search algorithms section

--- Searching/Search_Binary.js
+++ Searching/Search_Binary.js
@@ -1,7 +1,27 @@
 // the function accepts a sorted array and a value
-// create a left pointer at the start of the array, and a right pointer at the end of the array// While the leftpointer comes before the right pointer:/
-// create a pointer in the middle
-//if you find the value you want, return the index
-// if the value is too small, move the left pointer up
-// if the value is too large, move the right pointer down
-// if you never find the value, return -1
+const searchBinary = (arr, val) => {
+    // create a left pointer at the start of the array, and a right pointer at the end of the array
+    let start = 0;
+    let end = arr.length - 1;
+    let middle = Math.floor((start + end) / 2);
+    // While the leftpointer comes before the right pointer:/
+    while (arr[middle] !== val && start <= end) {
+        //if you find the value you want, return the index
+        if (val < arr[middle]) {
+            // if the value is too small, move the left pointer up
+            end = middle - 1;
+        } else {
+            start = middle + 1;
+        }
+        console.log(start, middle, end);
+        // create a pointer in the middle
+        middle = Math.floor((start + end) / 2);
+        // if the value is too large, move the right pointer down
+        if (arr[middle] === val) {
+            return middle;
+        }
+        // if you never find the value, return -1
+        return -1
+    };
+}
+console.log(searchBinary([30, 32, 34, 36, 38, 40, 42, 44, 46, 48, 50], 36));
---
+++ Searching/Search_String.js
@@ -0,0 +1,26 @@
+ // function that accepts two strings
+ const searchString = (long, short) => {
+     let count = 0;
+     // Loop over the longer string
+     for (let i = 0; i < long.length; i++) {
+         // loop over the shorter string
+         for (let j = 0; j < short.length; j++) {
+             console.log(short[i], long[i + j]);
+             // if thecharacters don't match, break out of the inner loop
+             if (short[j] !== long[i + j]) {
+                 console.log("Break!!!")
+                 break;
+             }
+             // if the characters do match, keep going
+             if (j === short.length - 1) {
+                 console.log("FOUND ONE!")
+                     // if you complete the inner loop and find a match, increment the count of matches
+                 count++;
+             }
+         }
+     }
+     // return the count
+     return count
+ }
+
+ console.log(searchString("lori loled ", "lol"));
